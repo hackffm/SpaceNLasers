27b4ff92af88dbd19910fdd3519f83d4fa7a6599
hephaisto <hephaisto@users.noreply.github.com>
2016-01-12 22:53:00 +0100

    AG from 'per-target score value' (dd72f04a668cfae185b4d8cf57663923b93688bd)

--- doc/html/search/all_10.js
+++ doc/html/search/all_10.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['s',['s',['../classlib_1_1MenuGod_1_1MenuGod.html#a25401fd260631768b22d80e0dd61f970',1,'lib::MenuGod::MenuGod']]],
   ['scores',['scores',['../classgamemodes_1_1Domination_1_1Gamemode.html#aec3228d29e3ec3acc3d5a950b9c16377',1,'gamemodes.Domination.Gamemode.scores()'],['../classgamemodes_1_1ShootingGallery_1_1Gamemode.html#acfc99211f9548f6c2e67b97634366bc5',1,'gamemodes.ShootingGallery.Gamemode.scores()']]],
+  ['scorevalue',['scoreValue',['../classlib_1_1HardwareTarget_1_1HardwareTarget.html#aba8518f2438dee7af1e00e81393c7bb0',1,'lib::HardwareTarget::HardwareTarget']]],
   ['senderror',['SendError',['../classlib_1_1MenuGod_1_1MenuGod.html#a8aeb02352b20605d2fbea2c640a2cb8e',1,'lib::MenuGod::MenuGod']]],
   ['sendgameinfo',['SendGameInfo',['../classlib_1_1MenuGod_1_1FakeMenuGod.html#a545090667d6356d8d857e04387c6d4ba',1,'lib.MenuGod.FakeMenuGod.SendGameInfo()'],['../classlib_1_1MenuGod_1_1MenuGod.html#a8c0773034b9e0c120b6d10acaf74bf8c',1,'lib.MenuGod.MenuGod.SendGameInfo()']]],
   ['serialhalfduplex',['SerialHalfDuplex',['../classlib_1_1SerialHalfDuplex_1_1SerialHalfDuplex.html',1,'lib::SerialHalfDuplex']]],
--- doc/html/search/variables_e.js
+++ doc/html/search/variables_e.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['s',['s',['../classlib_1_1MenuGod_1_1MenuGod.html#a25401fd260631768b22d80e0dd61f970',1,'lib::MenuGod::MenuGod']]],
   ['scores',['scores',['../classgamemodes_1_1Domination_1_1Gamemode.html#aec3228d29e3ec3acc3d5a950b9c16377',1,'gamemodes.Domination.Gamemode.scores()'],['../classgamemodes_1_1ShootingGallery_1_1Gamemode.html#acfc99211f9548f6c2e67b97634366bc5',1,'gamemodes.ShootingGallery.Gamemode.scores()']]],
+  ['scorevalue',['scoreValue',['../classlib_1_1HardwareTarget_1_1HardwareTarget.html#aba8518f2438dee7af1e00e81393c7bb0',1,'lib::HardwareTarget::HardwareTarget']]],
   ['shotcode',['shotCode',['../classlib_1_1Weapon_1_1Weapon.html#a9016940ed76668dd27274a426a5e3120',1,'lib::Weapon::Weapon']]],
   ['soundfiles',['soundFiles',['../namespaceGameMaster.html#a0f9037d20a01840cfad42fdb6080d632',1,'GameMaster']]],
   ['sounds',['sounds',['../classlib_1_1GameEngine_1_1GameEngine.html#ab230b5897f2ac60ec3a3231ab7f8e7a5',1,'lib.GameEngine.GameEngine.sounds()'],['../classlib_1_1GameWorld_1_1GameWorld.html#a9b848c63c10344874b3760515e5849a7',1,'lib.GameWorld.GameWorld.sounds()'],['../namespaceGameMaster.html#aefb99e3c560fac3a3a4b7768d6f4a51a',1,'GameMaster.sounds()']]],
