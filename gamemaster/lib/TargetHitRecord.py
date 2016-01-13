a7b32223803a50219d2fbb144b64d0237253e6af
hephaisto <hephaisto@users.noreply.github.com>
2016-01-13 04:58:13 +0100

    removed legacy files

--- gamemaster/lib/TargetHitRecord.py
+++
@@ -1,9 +0,0 @@
-class TargetHitRecord:
-	weapon = ''
-	hitPoint = 5 # default hit point
-	hitType = 'laser' # default hit type 
-
-	def __init__(self,weapon,hitPoint,hitType):
-		self.weapon = weapon
-		self.hitPoint = hitPoint
-		self.hitType = hitType
