13b12d7ae669072de6d5cba9abf920f327aed8ea
hephaisto <hephaisto@users.noreply.github.com>
2016-01-19 01:05:26 +0100

    softer rumble

--- gamemaster/lib/BusFactory.py
+++ gamemaster/lib/BusFactory.py
@@ -14,7 +14,7 @@ def StartShootingSequence():
 	return "S\n"
 
 def RumbleShootAnimation(weaponLetter):
-	return "{}A011FF000401\n".format(weaponLetter)
+	return "{}A01180000401\n".format(weaponLetter)
 
 def DoSomethingAnimationLikeOnWeapon(weaponLetter): # TODO: please fix name here and in calling function
 	return "{}a00704\n".format(weaponLetter)
